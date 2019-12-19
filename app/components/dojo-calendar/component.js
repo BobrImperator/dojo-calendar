7ebcb2da4596a80c5a4fa52bc7245cfd7e590cb4
BobrImperator <bartholomew.dudzik@gmail.com>
2019-12-19 17:14:45 +0100

    rendering in loop

--- app/components/dojo-calendar/component.js
+++ app/components/dojo-calendar/component.js
@@ -1,9 +1,18 @@
 import Component from "@glimmer/component";
+import { tracked } from '@glimmer/tracking';
 
 export default class DojoCalendar extends Component {
-  helloFromComponent = "Hi"
+  @tracked dates;
 
-  get getHello() {
-    return this.args.hello;
+  constructor() {
+    super(...arguments);
+    const dates  = [];
+
+    for (let index = 1; index <= 31; index++) {
+      const date = `2019-12-${index}`
+      dates.push(date);
+    }
+
+    this.dates = dates;
   }
 }
